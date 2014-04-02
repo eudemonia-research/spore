42dcd11ae9887645b898afcf7fffb5a6f0c8c28d
Kitten Tofu <user.name>
2014-04-02 13:27:41 +1100

    changed lighthouse -> server and ship -> client in networking test

--- tests/tests.py
+++ tests/tests.py
@@ -8,33 +8,33 @@ class TestNetworking(unittest.TestCase):
 
     def setUp(self):
         self.port = random.randint(10000, 60000)
-        self.lighthouse = Spore(seeds=[], address=('127.0.0.1', self.port))
-        self.lighthouse.start()
-        self.ship = Spore(seeds=[('127.0.0.1', self.port)])
-        self.ship.start()
+        self.server = Spore(seeds=[], address=('127.0.0.1', self.port))
+        self.server.start()
+        self.client = Spore(seeds=[('127.0.0.1', self.port)])
+        self.client.start()
 
     def tearDown(self):
-        self.lighthouse.stop()
-        self.ship.stop()
+        self.server.stop()
+        self.client.stop()
 
     def test_connection(self):
         print("Sleep for 100ms to give them time to connect")
         time.sleep(0.1)
 
         print("Test that they are connected to each other.")
-        self.assertEqual(self.ship.num_connected_peers(), 1)
-        self.assertEqual(self.lighthouse.num_connected_peers(), 1)
+        self.assertEqual(self.client.num_connected_peers(), 1)
+        self.assertEqual(self.server.num_connected_peers(), 1)
 
         print("Stop them")
-        self.lighthouse.stop()
-        self.ship.stop()
+        self.server.stop()
+        self.client.stop()
 
         print("Sleep for 100ms let them clean up")
         time.sleep(0.1)
 
         print("Test that they are no longer connected to each other.")
-        self.assertEqual(self.lighthouse.num_connected_peers(), 0)
-        self.assertEqual(self.ship.num_connected_peers(), 0)
+        self.assertEqual(self.server.num_connected_peers(), 0)
+        self.assertEqual(self.client.num_connected_peers(), 0)
 
         print("Ensure there are no other threads running.")
         self.assertEqual(threading.active_count(), 1)
@@ -43,15 +43,15 @@ class TestNetworking(unittest.TestCase):
         time.sleep(0.1)
 
         print("Start them again")
-        self.lighthouse.start()
-        self.ship.start()
+        self.server.start()
+        self.client.start()
 
         print("Sleep again")
         time.sleep(0.1)
 
         print("Test that they are connected to each other.")
-        self.assertEqual(self.ship.num_connected_peers(), 1)
-        self.assertEqual(self.lighthouse.num_connected_peers(), 1)
+        self.assertEqual(self.client.num_connected_peers(), 1)
+        self.assertEqual(self.server.num_connected_peers(), 1)
 
 if __name__ == '__main__':
     unittest.main()
