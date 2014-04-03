11928580aca14b4c651d07652a221cd38182e3bb
Kitten Tofu <user.name>
2014-04-03 19:42:33 +1100

    added tests for broadcasts

--- tests/tests.py
+++ tests/tests.py
@@ -13,46 +13,98 @@ class TestNetworking(unittest.TestCase):
         self.client = Spore(seeds=[('127.0.0.1', self.port)])
         threading.Thread(target=self.server.run).start()
         threading.Thread(target=self.client.run).start()
+        # Sleep for 150ms to give them time to connect
+        time.sleep(0.15)
 
     def tearDown(self):
         self.server.shutdown()
         self.client.shutdown()
-
-    def test_connection(self):
-        print("Sleep for 150ms to give them time to connect")
+        # Sleep for 150ms to give them time to shutdown
         time.sleep(0.15)
 
-        print("Test that they are connected to each other.")
+    def test_connection(self):
+
+        #print("Test that they are connected to each other.")
         self.assertEqual(self.client.num_connected_peers(), 1)
         self.assertEqual(self.server.num_connected_peers(), 1)
 
-        print("Stop them")
+        #print("Stop them")
         self.server.shutdown()
         self.client.shutdown()
 
-        print("Sleep for 150ms let them clean up")
+        #print("Sleep for 150ms let them clean up")
         time.sleep(0.15)
 
-        print("Test that they are no longer connected to each other.")
+        #print("Test that they are no longer connected to each other.")
         self.assertEqual(self.server.num_connected_peers(), 0)
         self.assertEqual(self.client.num_connected_peers(), 0)
 
-        print("Ensure there are no other threads running.")
+        #print("Ensure there are no other threads running.")
         self.assertEqual(threading.active_count(), 1)
 
-        print("Sleep 150ms")
+        #print("Sleep 150ms")
         time.sleep(0.15)
 
-        print("Start them again")
+        #print("Start them again")
         threading.Thread(target=self.server.run).start()
         threading.Thread(target=self.client.run).start()
 
-        print("Sleep again")
+        #print("Sleep again")
         time.sleep(0.15)
 
-        print("Test that they are connected to each other.")
+        #print("Test that they are connected to each other.")
         self.assertEqual(self.client.num_connected_peers(), 1)
         self.assertEqual(self.server.num_connected_peers(), 1)
+
+
+class TestSimpleMessages(unittest.TestCase):
+
+    def setUp(self):
+        """ Create a client and server, and run them in their own threads. """
+        self.port = random.randint(10000, 60000)
+        self.server = Spore(seeds=[], address=('127.0.0.1', self.port))
+        self.client = Spore(seeds=[('127.0.0.1', self.port)])
+        threading.Thread(target=self.server.run).start()
+        threading.Thread(target=self.client.run).start()
+        # Sleep for 150ms to give them time to connect
+        time.sleep(0.15)
+
+    def tearDown(self):
+        self.server.shutdown()
+        self.client.shutdown()
+        # Sleep for 150ms to give them time to shutdown
+        time.sleep(0.15)
+
+    def test_messages(self):
+        client_received_message = False
+        server_received_message = False
+
+        # Checks encoding.
+        MAGIC_FOR_SERVER = {'true':['1234','false']}
+        MAGIC_FOR_CLIENT = {'4321':['false']}
+
+        @self.server.handler
+        def client_to_server(peer, params):
+            self.assertEqual(params['magic_for_server'], MAGIC_FOR_SERVER)
+            server_received_message = True
+
+        @self.client.handler
+        def server_to_client(peer, params):
+            self.assertEqual(params['magic_for_client'], MAGIC_FOR_CLIENT)
+            client_received_message = True
+
+        self.server.broadcast('client_to_server', {'magic_for_server': MAGIC_FOR_SERVER})
+        self.client.broadcast('server_to_client', {'magic_for_client': MAGIC_FOR_CLIENT})
+        time.sleep(0.1)
+        self.assertFalse(client_received_message)
+        self.assertFalse(server_received_message)
+
+        self.server.broadcast('server_to_client', {'magic_for_client': MAGIC_FOR_CLIENT})
+        self.client.broadcast('client_to_server', {'magic_for_server': MAGIC_FOR_SERVER})
+        time.sleep(0.2)
+        self.assertTrue(client_received_message)
+        self.assertTrue(server_received_message)
+
 
 if __name__ == '__main__':
     unittest.main()
