48ff95e4949c406702a76b7dfa90c2203749d55e
Kitten Tofu <kitten@eudemonia.io>
2014-05-06 13:50:31 +1000

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='spore',
-      version='0.9.3',
+      version='0.9.4',
       description='Simple P2P Framework',
       author='Kitten Tofu',
       author_email='kitten@eudemonia.io',
